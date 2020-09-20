9f13ee5d8434a1cf1f6adb264338a4a570c81f9e
Mariano Paniagua <mpaniagua@contractor.ea.com>
2020-09-20 19:12:43 -0300

    Updated db

--- the_cave.sql
+++ the_cave.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2020 at 12:32 AM
+-- Generation Time: Sep 21, 2020 at 12:11 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.31
 
@@ -29,8 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `color` varchar(100) NOT NULL,
-  `id_category` varchar(100) NOT NULL
+  `name` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`color`, `name`, `id`) VALUES
+('red', 'processor', 0),
+('blue', 'ram', 1),
+('green', 'cooler', 2);
 
 -- --------------------------------------------------------
 
@@ -43,7 +53,7 @@ CREATE TABLE `products` (
   `name` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL,
   `price` float NOT NULL,
-  `id_category` varchar(100) NOT NULL
+  `id_category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -51,10 +61,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `price`, `id_category`) VALUES
-(1, 'Procesador Intel Core i7 9700F 4.7GHz Turbo 1151 Coffee Lake', 'intel-core-i7.jpg', 35.999, ''),
-(2, 'Procesador AMD RYZEN 7 3700X 4.4 GHz AM4 Wraith Prism RGB Led Cooler', 'ryzen-7.jpg', 35.04, ''),
-(3, 'Procesador Intel Core i7 9700F 4.7GHz Turbo 1151 Coffee Lake', 'intel-core-i7.jpg', 35.999, ''),
-(4, 'Procesador AMD RYZEN 7 3700X 4.4 GHz AM4 Wraith Prism RGB Led Cooler', 'ryzen-7.jpg', 35.04, '');
+(1, 'Procesador Intel Core i7 9700F 4.7GHz Turbo 1151 Coffee Lake', 'intel-core-i7.jpg', 35.999, 0),
+(2, 'Procesador AMD RYZEN 7 3700X 4.4 GHz AM4 Wraith Prism RGB Led Cooler', 'ryzen-7.jpg', 35.04, 0),
+(6, 'Cooler CPU DeepCool Gammaxx 400S', 'Cooler CPU DeepCool Gammaxx 400S.jpg', 5280, 2),
+(7, 'Cooler CPU AZZA Blizzard LCAZ 120R WaterCooler 120mm ARGB', 'Cooler CPU AZZA Blizzard LCAZ 120R WaterCooler 120mm ARGB.jpg', 10680, 2),
+(8, 'Cooler CPU ID-Cooling AURAFLOW X 360', 'Cooler CPU ID-Cooling AURAFLOW X 360.jpg', 9876, 2);
 
 --
 -- Indexes for dumped tables
@@ -64,15 +75,15 @@ INSERT INTO `products` (`id`, `name`, `image`, `price`, `id_category`) VALUES
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id_category`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`id_category`),
-  ADD KEY `id` (`id`);
+  ADD KEY `id` (`id`),
+  ADD KEY `id_category` (`id_category`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -82,17 +93,17 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `categories`
+-- Constraints for table `products`
 --
-ALTER TABLE `categories`
-  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `products` (`id_category`);
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
