ff60fa9fcc1a91699cb88e44716611787693076f
Mariano Paniagua <mpaniagua@contractor.ea.com>
2020-09-30 19:40:21 -0300

    merge conflicts

--- utils/router.php
+++ utils/router.php
@@ -2,16 +2,26 @@
    require_once 'RouterClass.php';
    require_once '../controller/ProductsController.php';
    require_once '../controller/LoginController.php';
+   require_once '../controller/ContactController.php';
+   require_once '../controller/HomeController.php';
    require_once '../controller/AdminController.php';
+
 
     define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
 
-    $r = new Router(new Route("home", "GET", "ProductsController", "home"));
-    $r->addRoute("home", "GET", "ProductsController", "home");
+    $r = new Router();
+    $r->setDefaultRoute("HomeController", "showHome");
+    $r->addRoute("home", "GET", "HomeController", "showHome");
     $r->addRoute("login", "POST", "LoginController", "login");
     $r->addRoute("login", "GET", "LoginController", "showLoginForm");
-    $r->addRoute("signup", "GET", "AdminController", "showAdminForm");
-    $r->addRoute("signup", "POST", "AdminController", "signup");
-    
-    
+    $r->addRoute("signup", "GET", "LoginController", "showSignUpForm");
+    $r->addRoute("signup", "POST", "LoginController", "signUp");
+    $r->addRoute("logout", "GET", "LoginController", "logOut");
+    $r->addRoute("contact", "GET", "ContactController", "showContactForm");
+    $r->addRoute("products", "GET", "ProductsController", "showProducts");
+    $r->addRoute("productsAdmin", "GET", "AdminController", "showAdmin");
+    $r->addRoute("add-product", "POST", "ProductsController", "addProduct");
+    $r->addRoute("delete-product", "POST", "ProductsController", "deleteProduct");
+    $r->addRoute("purchases", "GET", "AdminController", "showPurchases");
+
     $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']);
