40786864ceec8665ee36ad1061bc0dfb7023aea6
Juan Francisco Vaninetti <franciscovaninetti@gmail.com>
2020-09-30 22:01:20 -0300

    bad merge

--- utils/router.php
+++ utils/router.php
@@ -6,20 +6,12 @@
 
     define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
 
-    $r = new Router();
-    $r->setDefaultRoute("HomeController", "showHome");
-    $r->addRoute("home", "GET", "HomeController", "showHome");
+    $r = new Router(new Route("home", "GET", "ProductsController", "home"));    
+    $r->addRoute("home", "GET", "ProductsController", "home");
     $r->addRoute("login", "POST", "LoginController", "login");
     $r->addRoute("login", "GET", "LoginController", "showLoginForm");
-    $r->addRoute("signup", "GET", "LoginController", "showSignUpForm");
-    $r->addRoute("signup", "POST", "LoginController", "signUp");
-    $r->addRoute("contact", "GET", "ContactController", "showContactForm");
-    $r->addRoute("products", "GET", "ProductsController", "showProducts");
-    $r->addRoute("admin", "GET", "AdminController", "showAdmin");
-    $r->addRoute("add-product", "POST", "AdminController", "addProduct");
-    $r->addRoute("modify-product", "POST", "AdminController", "modifyProduct");
-    $r->addRoute("delete-product", "POST", "ProductsController", "deleteProduct");
-
+    $r->addRoute("signup", "GET", "AdminController", "showAdminForm");
+    $r->addRoute("signup", "POST", "AdminController", "signup");
     
     
     $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']);
